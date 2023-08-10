66c637da46e3a1e3361dfca8cea2ba747d129e2b
TeresaOuO <tzutungchou@gmail.com>
2023-08-10 12:18:08 +0800

    Create project

---
+++ rabbit_game1/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace rabbit_game1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtShow = new System.Windows.Forms.TextBox();
+            this.rabbit = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.rabbit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(233, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "命中";
+            // 
+            // txtShow
+            // 
+            this.txtShow.ForeColor = System.Drawing.Color.Red;
+            this.txtShow.Location = new System.Drawing.Point(290, 39);
+            this.txtShow.Name = "txtShow";
+            this.txtShow.Size = new System.Drawing.Size(100, 22);
+            this.txtShow.TabIndex = 1;
+            // 
+            // rabbit
+            // 
+            this.rabbit.Image = global::rabbit_game1.Properties.Resources.rabbit;
+            this.rabbit.Location = new System.Drawing.Point(161, 163);
+            this.rabbit.Name = "rabbit";
+            this.rabbit.Size = new System.Drawing.Size(144, 167);
+            this.rabbit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.rabbit.TabIndex = 2;
+            this.rabbit.TabStop = false;
+            this.rabbit.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(409, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "次數";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.ForeColor = System.Drawing.Color.Red;
+            this.txtNumber.Location = new System.Drawing.Point(466, 38);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 22);
+            this.txtNumber.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(803, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(29, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "等級";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "初級",
+            "中級",
+            "高級"});
+            this.comboBox1.Location = new System.Drawing.Point(96, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "請選擇";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(593, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 19);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "分數";
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(659, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1219, 544);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rabbit);
+            this.Controls.Add(this.txtShow);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.rabbit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtShow;
+        private System.Windows.Forms.PictureBox rabbit;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ rabbit_game1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rabbit_game1
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random(2);
+        int number = 0;
+        int score = 0;
+        int second = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            txtShow.Text = "Oh!!";
+            number = number + 1;
+            score = score + 10;
+            txtNumber.Text = number.ToString();
+            textBox1.Text = score.ToString();
+            if (number >= 10) {
+                timer1.Enabled = false;
+                rabbit.Visible = false;
+                MessageBox.Show("恭喜你過關!你一共用了"+second+"秒");
+                txtShow.Text = "";
+                txtNumber.Text = "";
+                textBox1.Text = "";
+                comboBox1.Text = "請選擇";
+                timer1.Enabled = false;
+                rabbit.Visible = false;
+                comboBox1.Enabled = true;
+                button1.Enabled = false;
+                timer1.Enabled = false;
+                number = 0;
+                score = 0;
+                second = 0;
+            }
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            txtShow.Text = "";
+            int x;
+            int y;
+            x = rnd.Next(1, 1073);
+            y = rnd.Next(1, 377);
+            rabbit.Location = new Point(x, y);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtShow.Text = "";
+            txtNumber.Text = "";
+            timer1.Enabled = false;
+            rabbit.Visible = false;
+            button1.Enabled = false;
+            timer2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            rabbit.Visible = true;
+            comboBox1.Enabled = false;
+            button1.Enabled = false;
+            timer2.Enabled = true;
+            timer2.Interval = 1000;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int i;
+            i = comboBox1.SelectedIndex;
+            switch (i)
+            {
+                case -1:
+                    MessageBox.Show("請選擇難易度");
+                    break;
+                case 0:
+                    timer1.Interval = 1000;
+                    
+                    break;
+                case 1:
+                    timer1.Interval = 500;
+                    
+                    break;
+                case 2:
+                    timer1.Interval = 200;
+                    
+                    break;
+            }
+            button1.Enabled = true;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            second = second + 1;
+        }
+    }
+}
